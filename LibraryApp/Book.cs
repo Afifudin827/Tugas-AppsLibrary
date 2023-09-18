c32caf0d19e4e54de1b73a254c3a99cb0d13e070
Afifudin <afifudin.card@gmail.com>
2023-09-18 10:55:07 +0700

    Initial commit

---
+++ LibraryApp/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp;
+class Book
+{
+    private string title { get; set; }
+    private string author {  get; set; }
+    private string publicationYear {  get; set; }
+}
---
+++ LibraryApp/LibraryCatalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp;
+class LibraryCatalog : Book
+{
+    public static bool addBook(Book book)
+    {
+
+        return true;
+    }
+
+    public static bool removeBook(Book book) 
+    {  
+        return true; 
+    }
+    public static void findBook(string title) 
+    {
+    }
+    public static void listBook()
+    {
+
+    }
+}
